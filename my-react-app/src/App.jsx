ab378c6367ff258461b02ca01cf6db4f7aa88012
Aer054 <112958865+Aer054@users.noreply.github.com>
2024-04-24 15:36:22 +0700

    upd

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Diamant from './games/diamant/Diamant.jsx'
 import ListOfGames from './ListOfGames'
 import { GameProvider } from './games/diamant/GameContext.jsx';
 import Counter from './games/skull/Counter.jsx';
+import LobbyList from './lobby/src/LobbyList.jsx'
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,8 @@ function App() {
         <Route path="Diamant" element={ <GameProvider><Diamant/></GameProvider>} />
         <Route path="Skull" element={<Counter/>} />
         <Route path="/" element={<ListOfGames/>} />
+        <Route path="LobbyList" element={ <LobbyList nickname="User" avatar={3} background={2} />} />
+       
       </Routes>
     </BrowserRouter>
   );
